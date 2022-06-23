11826924c8c158eecb2a8dd4fea4797b315b65d2
haruta <a8120203@gsuite.si.aoyama.ac.jp>
2022-06-23 17:35:42 +0900

    リセット関連の処理

--- yamanote/yamanote.js
+++ yamanote/yamanote.js
@@ -1,5 +1,5 @@
 async function resetGame() {
     var sql = `update Game set turn = 0 where id = 1;`;
     var objects = await osql.connect(sql);
-    location.href = 'game.html';
+    location.href = 'waiting.html';
 }
