813539806ccc2db2450d6796e9fb4eeecfad2265
Momi Sato <a8120096@gsuite.si.aoyama.ac.jp>
2022-06-23 19:45:59 +0900

    doInsertの変更

--- yamanote/yamanote.js
+++ yamanote/yamanote.js
@@ -1,8 +1,8 @@
-
-
 async function resetGame() {
-    var sql = `update Game set turn = 0 where id = 1;`;
+    var sql = `update Game set turn = 0 , playernumber = 0 where id = 1;`;
     var objects = await osql.connect(sql);
     location.href = 'waiting.html';
+    var sql = `delete from PlayerGame where gameid = 1`;
+    var objects = await osql.connect(sql);
 }
 
